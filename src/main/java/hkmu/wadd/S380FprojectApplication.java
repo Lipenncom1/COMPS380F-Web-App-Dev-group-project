de6087d9c8ef494025ed255e3faa46a289648518
Lipenncom1 <148969818+Lipenncom1@users.noreply.github.com>
2025-03-25 01:11:21 +0800

    New SpringBoot frame

---
+++ src/main/java/hkmu/wadd/S380FprojectApplication.java
@@ -0,0 +1,13 @@
+package hkmu.wadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S380FprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S380FprojectApplication.class, args);
+    }
+
+}
